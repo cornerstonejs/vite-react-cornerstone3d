f73f1ee92283e2777a75af4be7ec085d3a43511a
Alireza <ar.sedghi@gmail.com>
2024-06-19 17:34:19 -0400

    final build works with everything

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,17 @@
-import { defineConfig, searchForWorkspaceRoot } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-
-// IMPORTANT: use the rollup wasm since vite use rollup for the build
-// and esbuild during the dev and the rollup plugin seems to work
-// for both dev and build
-import wasmRollup from "@rollup/plugin-wasm"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasmRollup()],
+  plugins: [react()],
   worker: {
     format: "es",
-    plugins: () => [wasmRollup()],
   },
   server: {
-    // fs: {
-    //   allow: [
-    //     searchForWorkspaceRoot(import.meta.url),
-    //     // "/Users/alireza/dev/admin/cornerstone3D.git.worktrees/beta/packages/dicomImageLoader/dist/esm/src/decodeImageFrameWorker.js",
-    //     // "/Users/alireza/dev/admin/cornerstone3D.git.worktrees/beta/node_modules/@cornerstonejs/codec-charls",
-    //     // "@cornerstonejs/dicom-image-loader",
-    //   ],
-    // },
-    // set the cross-origin resource sharing headers
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
     },
-  },
-  optimizeDeps: {
-    exclude: ["@cornerstonejs/dicom-image-loader"],
   },
   assetsInclude: ["**/*.wasm"],
 })
